c766f49476f2b8b51f96e5d58cc3671832009911
VeeraMahendra7 <140475884+VeeraMahendra7@users.noreply.github.com>
2025-11-14 13:01:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+
+
+let input1 = parseInt(prompt('Enter the first number: '));
+let input2 = parseInt(prompt('Enter the second number: '));
+
+let result = (input1*2)+input2;
+
+alert(`The final result is: ${result}`);
